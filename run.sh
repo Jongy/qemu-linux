9a21773ddbecfd62a6152058d387c0d7ddd230ae
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2019-12-22 01:31:52 +0200

    run: Pass parameters directly to qemu, without expanding them in shell variables
    Expanding and expanding removes quotes and double quotes, like those used
    in "-append". Need them unexpanded.

--- run.sh
+++ run.sh
@@ -12,14 +12,11 @@ mkdir -p fs
 fallocate -l 30M ext4
 mkfs.ext4 -F ext4 -d fs
 
-QEMU_OPTS="-kernel $1"
-QEMU_OPTS="$QEMU_OPTS -enable-kvm"
-QEMU_OPTS="$QEMU_OPTS -initrd initramfs.cpio.gz"
-QEMU_OPTS="$QEMU_OPTS -nographic -append \"console=ttyS0\""
-QEMU_OPTS="$QEMU_OPTS -drive file=ext4,format=raw"
-
-QEMU_OPTS="$QEMU_OPTS -netdev tap,id=net0,ifname=tap-qemu,script=configure_tap_qemu.sh,downscript=no"
-QEMU_OPTS="$QEMU_OPTS -device e1000,netdev=net0"
-
-echo "QEMU opts: $QEMU_OPTS"
-sudo qemu-system-x86_64 $QEMU_OPTS
+sudo qemu-system-x86_64 \
+    -kernel "$1" \
+    -enable-kvm \
+    -initrd initramfs.cpio.gz \
+    -nographic -append "console=ttyS0" \
+    -drive file=ext4,format=raw \
+    -netdev tap,id=net0,ifname=tap-qemu,script=configure_tap_qemu.sh,downscript=no \
+    -device e1000,netdev=net0
